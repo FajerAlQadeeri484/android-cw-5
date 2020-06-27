0d43d65b2619f91431bdbb7ec4401c2e0129243c
fajernbq <66422481+fajernbq@users.noreply.github.com>
2020-06-27 19:06:43 +0300

    Cw5Done

---
+++ CW5/app/src/main/java/com/example/cw5/MainActivity2.java
@@ -0,0 +1,19 @@
+package com.example.cw5;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        TextView n =findViewById(R.id.textView2);
+        Bundle b= getIntent().getExtras();
+        
+    }
+}
